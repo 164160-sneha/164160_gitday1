09c35e0d8e6076f8af60fa74c2f4dc5a1a183cd4
sneha <jksnehajm@gmail.com>
2018-11-02 16:12:49 +0530

    java project

---
+++ src/org/corp/Sample1.java
@@ -0,0 +1,10 @@
+package org.corp;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Java demo program");
+	}
+
+}
