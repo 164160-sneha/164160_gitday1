af346e44eddefde7ed81cddcbb3a2838750f444e
164160-sneha <44638784+164160-sneha@users.noreply.github.com>
2019-02-15 16:29:14 +0530

    Add files via upload

---
+++ express-nodejs/assignment21.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+app.get('/year', function(req, res){
+var age=req.query.age;
+var date=new Date();
+var currentYear= date.getFullYear();
+var diff = currentYear-age;
+   res.send('You were born in ' +diff);
+});
+app.listen(8000);
